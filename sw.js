c7d8c1b3b9e2cf914f02eef63bcfc0dc4af9602a
rayserit <82518104+rayserit@users.noreply.github.com>
2025-11-18 23:47:44 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = 'tv-tracker-v1';
+const ASSETS_TO_CACHE = [
+  './serietv_tracker.html',
+  './manifest.json',
+  './icon.svg'
+];
+
+// Installazione
+self.addEventListener('install', (event) => {
+  self.skipWaiting();
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS_TO_CACHE);
+    })
+  );
+});
+
+// Attivazione e pulizia
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Fetch
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
+});
